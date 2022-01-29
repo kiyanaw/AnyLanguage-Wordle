d3e61a720f1984ab74beb8552427d06d5d96fae9
Aaron Fay <aaron.j.fay@gmail.com>
2022-01-29 13:40:15 -0800

    fix name, shuffle off

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -1,6 +1,6 @@
 export const CONFIG = {
   tries: 6, // This changes how many tries you get to finish the wordle
-  language: 'nêyiyawêwin', // This changes the display name for your language
+  language: 'nêhiyawêwin', // This changes the display name for your language
   wordLength: 5, // This sets how long each word is based on how many characters (as defined in orthography.ts) are in each word
   author: 'kiyânaw', // Put your name here so people know who made this Wordle!
   authorWebsite: 'https://www.facebook.com/kiyanawapps', // Put a link to your website or social media here
@@ -10,6 +10,6 @@ export const CONFIG = {
   // THESE NEXT SETTINGS ARE FOR ADVANCED USERS
   //
   googleAnalytics: '', // You can use this if you use Google Analytics
-  shuffle: true, // whether to shuffle the words in the wordlist
+  shuffle: false, // whether to shuffle the words in the wordlist
   normalization: 'NFC', // whether to apply Unicode normalization to words and orthography - options: 'NFC', 'NFD', 'NKFC', 'NKFD', false
 }
